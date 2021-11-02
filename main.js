2ba334de0c7e97321780dfe9445c4fccaffe25e5
Alisa-InWonderland <alisa_ne@yahoo.com>
2021-11-02 15:56:07 +0100

    hecho

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+	let tasks = [];
+
+	$('#submit').on("click", function () {
+		let newTask = $('#newTask').val();
+
+		if (newTask !== "") {
+
+			tasks.push(newTask);
+
+			$('#taskList').empty()
+
+			tasks.forEach(t =>
+				$('#taskList').append(
+					`<div class="task">${t}</div>`
+				));
+		}
+
+		let oldTask = $('#newTask').val(''); /* para borrar los valores de newTask despues de haberlos añadido en la lista de tareas*/
+
+	})
+
+	$('#clearButton').on("click", function () {
+
+		$(".task").remove();
+		tasks.length = 0;
+	})
+});
